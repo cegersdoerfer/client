195fbbf4133e657b74bf1188dd96725ec6e318bc
Chris Egersdoerfer <cegersdoerfer@gmail.com>
2024-09-24 22:21:29 -0400

    check second arg

--- workloads/IO500/run.sh
+++ workloads/IO500/run.sh
@@ -11,10 +11,16 @@ fi
 
 config_file="$1"
 
-# Check if Darshan tracing is enabled (second argument is a boolean)
-if [ "$2" = "true" ]; then
-    mpi_args="-np 4 -env DXT_ENABLE_IO_TRACE 1 -env LD_PRELOAD /custom-install/hpc-tools/pnetcdf-1.13.0/install/lib/libpnetcdf.so:/custom-install/hpc-tools/hdf5-1.14.4-3/install/lib/libhdf5.so:/custom-install/io-profilers/darshan-3.4.5/darshan-runtime/install/lib/libdarshan.so"
+# Check if there are two arguments
+if [ $# -eq 2 ]; then
+    # Check if Darshan tracing is enabled (second argument is a boolean)
+    if [ "$2" = "true" ]; then
+        mpi_args="-np 4 -env DXT_ENABLE_IO_TRACE 1 -env LD_PRELOAD /custom-install/hpc-tools/pnetcdf-1.13.0/install/lib/libpnetcdf.so:/custom-install/hpc-tools/hdf5-1.14.4-3/install/lib/libhdf5.so:/custom-install/io-profilers/darshan-3.4.5/darshan-runtime/install/lib/libdarshan.so"
+    else
+        mpi_args="-np 4"
+    fi
 else
+    # Default to no Darshan tracing if only one argument is provided
     mpi_args="-np 4"
 fi
 
