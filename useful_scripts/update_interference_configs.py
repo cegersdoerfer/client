7944fcad21834cc945cb760864c0a80d0bffd322
Chris Egersdoerfer <cegersdoerfer@gmail.com>
2024-09-24 23:05:15 -0400

    update configs

--- useful_scripts/update_interference_configs.py
+++ useful_scripts/update_interference_configs.py
@@ -13,8 +13,12 @@ def update_ini_files(directory):
                     config['global']['datadir'] = '/mnt/hasanfs/io500_runs/datafiles_interference'
                     config['global']['resultdir'] = '/mnt/hasanfs/io500_runs/results_interference'
                 
-                if 'ior-easy' in config:
-                    config['ior-easy']['blockSize'] = '10g'
+                #if 'ior-easy' in config:
+                #    config['ior-easy']['blockSize'] = '10g'
+                for section in config:
+                    if 'mdt' in section or 'mdw' in section:
+                        # Set the API to POSIX for all mdtest sections
+                        config[section]['API'] = 'POSIX'
 
                 with open(file_path, 'w') as configfile:
                     config.write(configfile)
