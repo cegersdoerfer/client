2872288b5be892543abf314602af70b7ed899a4b
Chris Egersdoerfer <cegersdoerfer@gmail.com>
2024-10-02 18:37:03 -0400

    edit config

--- workloads/amrex/run.sh
+++ workloads/amrex/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 config_file="$1"
 
-h5bench_dir="/custom-install/benchmarks/h5bench"
+h5bench_dir="/custom-install/benchmarks/h5bench/build"
 cd $h5bench_dir
 h5bench --debug $config_file
