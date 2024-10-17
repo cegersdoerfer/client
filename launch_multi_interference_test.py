58b044719e3790314614ebbfcbeef432411465e2
Chris Egersdoerfer <cegersdoerfer@gmail.com>
2024-10-17 12:10:09 -0400

    io500

--- launch_multi_interference_test.py
+++ launch_multi_interference_test.py
@@ -191,7 +191,7 @@ def main():
         DEBUG = False
 
     global username
-    workload = "macsio"
+    workload = "IO500"
     username = "root"
     if DEBUG:
         print("RUNNING IN DEBUG MODE")
@@ -208,7 +208,7 @@ def main():
     signal.signal(signal.SIGTERM, signal_handler)
 
     # Interference levels from 1 to 5
-    interference_levels = [0, 1, 2, 3, 4]
+    interference_levels = [0, 1, 2]
     assert 0 in interference_levels, "Interference level 0 is required"
     for interference_level in interference_levels:
         num_repetitions = 4
